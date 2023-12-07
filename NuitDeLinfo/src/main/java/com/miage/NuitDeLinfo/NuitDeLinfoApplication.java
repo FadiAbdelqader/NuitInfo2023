c89fd58ced2ae0303ffa0869cafd1316ab280353
FadiAbdelqader <abdelqader.fadi.h@gmail.com>
2023-12-07 21:34:42 +0100

    init

---
+++ NuitDeLinfo/src/main/java/com/miage/NuitDeLinfo/NuitDeLinfoApplication.java
@@ -0,0 +1,13 @@
+package com.miage.NuitDeLinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuitDeLinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuitDeLinfoApplication.class, args);
+	}
+
+}
---
+++ NuitDeLinfo/src/test/java/com/miage/NuitDeLinfo/NuitDeLinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.NuitDeLinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuitDeLinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
